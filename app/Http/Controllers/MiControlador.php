5f316a1c5b6dff04f47175b7fad51ca1ae5e9f74
gboned <gboned@cifpfbmoll.eu>
2021-01-15 17:29:24 +0100

    Añadir un nuevo controlador

---
+++ app/Http/Controllers/MiControlador.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MiControlador extends Controller
+{
+    function index() {
+        return "Clase MiControlador";
+    }
+}
