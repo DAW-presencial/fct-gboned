9ec3bac4d144f2c66a3c06580caed741abc455b3
gboned <gboned@cifpfbmoll.eu>
2021-01-17 04:18:30 +0100

    Añadir vistas y rutas nuevas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,12 @@
     <title>Home</title>
 </head>
 <body>
+    <ul>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/portfolio">Portfolio</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
     <h1>Home</h1>
         <p>Bienvenido/a <?php echo $nombre ?? "Invitado" ?></p>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('home', function() {
     return view('home')->with("nombre", $nombre);
 })->name('home');
 
+Route::view('/about', 'about')->name('about');
+Route::view('/portfolio', 'portfolio')->name('portfolio');
+Route::view('/contact', 'contact')->name('contact');
+
 Route::get('/', function () {
    // return view('welcome');
    echo "<a href='" . route('contacto') . "'>Contacto 1</a><br>";
