1285dcf7e1d85bd14c6a2a5551f236838a1fb65c
gboned <gboned@cifpfbmoll.eu>
2021-02-04 19:48:08 +0100

    Añadir navbar a página

---
+++ resources/views/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('title')
+    Form Result
+@endsection
+@section('content')
+    <h1>Form Result</h1>
+@endsection
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,16 +2,33 @@
 <html>
 <head>
     <title>@yield('title')</title>
+    {{-- Referencio BootStrap --}}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-    <nav>
-        <ul>
-            <li><a href="/home">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/portfolio">Portfolio</a></li>
-            <li><a href="/contact">Contact</a></li>
-            <li><a href="/form">Formularios</a></li>
-        </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/portfolio">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contact">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/form">Formularios</a>
+                </li>
     </nav>
     @yield('content')
 </body>
